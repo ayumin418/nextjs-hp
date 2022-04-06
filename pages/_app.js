2adb70e49214f4443a45af60e42899e1e5da1fa4
Ayumi Nagata <ayumito.n418@gmail.com>
2022-04-06 11:43:22 +0900

    [test]add react-gtm-module

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import TagManager from 'react-gtm-module';
+
+TagManager.initialize({ gtmId: 'GTM-NKFX3F8' });
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
